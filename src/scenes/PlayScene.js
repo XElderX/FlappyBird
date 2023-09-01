3f6f36d75dd7e5f5480e5cdcee739210edf1b9d1
XElderX <dalius.badley@gmail.com>
2023-09-01 18:35:24 +0300

    score functionality added

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -17,6 +17,9 @@ class PlayScene extends Phaser.Scene {
 
         this.VELOCITY = 200;
         this.flapVelocity = 250;
+
+        this.score = 0;
+        this.scoreText = '';
     }
     preload() {
         this.load.image('sky', 'assets/sky.png');
@@ -28,6 +31,7 @@ class PlayScene extends Phaser.Scene {
         this.createBird();
         this.createPipes();
         this.createColiders();
+        this.createScore();
         this.handleInputs();
     }
 
@@ -67,6 +71,11 @@ class PlayScene extends Phaser.Scene {
         this.physics.add.collider(this.bird, this.pipes, this.gameOver, null, this);
     }
 
+    createScore() {
+        this.score = 0;
+        this.scoreText = this.add.text(16, 16, `Score: ${0}`, { fontSize: '32px', fill: '#000' });
+    }
+
     handleInputs() {
         this.input.on('pointerdown', this.flap, this)
         this.input.keyboard.on('keydown_SPACE', this.flap, this)
@@ -99,6 +108,7 @@ class PlayScene extends Phaser.Scene {
                 tempPipes.push(pipe)
                 if (tempPipes.length === 2) {
                     this.placePipe(...tempPipes)
+                    this.increaseScore();
                 }
             }
         })
@@ -128,6 +138,10 @@ class PlayScene extends Phaser.Scene {
 
     flap() {
         this.bird.body.velocity.y = -this.flapVelocity;
+    }
+    increaseScore() {
+        this.score++;
+        this.scoreText.setText(`Score: ${this.score}` );
     }
 
 }
